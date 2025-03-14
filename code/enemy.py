078c6fea77791ae010a2c83d270b76dca148265c
Gabriel <gabriel.ferreira.santos.2001@gmail.com>
2025-03-13 21:33:07 -0300

    Adding classes to the project

---
+++ code/enemy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from Entity import Entity
+class Enemy:
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
---
+++ code/player.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player:
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
