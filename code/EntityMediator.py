dec78723b39347c59f8e3770cad0f52a334ca573
Gabriel <gabriel.ferreira.santos.2001@gmail.com>
2025-03-20 22:00:14 -0300

    Creating player and enemies shot

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,3 +1,6 @@
+from code.EnemyShot import EnemyShot
+from code.PlayerShot import PlayerShot
+from code.const import WIN_WIDTH
 from code.enemy import Enemy
 from code.entity import Entity
 
@@ -8,7 +11,12 @@ class EntityMediator:
         if isinstance(ent, Enemy):
             if ent.rect.right < 0:
                 ent.health = 0
-
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
+                ent.health = 0
     @staticmethod
     def verify_collision(entity_list: list[Entity]):
         for i in range(len(entity_list)):
