4eedc15d2b7e420b6f99d26b79f8f8485a142e38
Gabriel <gabriel.ferreira.santos.2001@gmail.com>
2025-03-24 21:02:08 -0300

    SQLite3 implementation for score using proxy design pattern

--- code/const.py
+++ code/const.py
@@ -126,9 +126,27 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 # S
 SPAWN_TIME = 4000
+
 # T
 TIMEOUT_STEP = 100  # 100ms
 TIMEOUT_LEVEL = 20000  # 20s
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+#  S
+SCORE_POS = {
+    'Title': (WIN_WIDTH / 3, 50),
+    'EnterName': (WIN_WIDTH / 3, 80),
+    'Label': (WIN_WIDTH / 3, 90),
+    'Name': (WIN_WIDTH / 3, 110),
+    0: (WIN_WIDTH / 3, 110),
+    1: (WIN_WIDTH / 3, 130),
+    2: (WIN_WIDTH / 3, 150),
+    3: (WIN_WIDTH / 3, 170),
+    4: (WIN_WIDTH / 3, 190),
+    5: (WIN_WIDTH / 3, 210),
+    6: (WIN_WIDTH / 3, 230),
+    7: (WIN_WIDTH / 3, 250),
+    8: (WIN_WIDTH / 3, 270),
+    9: (WIN_WIDTH / 3, 290),
+}
--- code/game.py
+++ code/game.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Score import Score
 from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.level import Level
 from code.menu import Menu
@@ -15,6 +16,7 @@ class Game:
     def run(self, ):
 
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -25,7 +27,10 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
-
+                    if level_return:
+                        score.save(menu_return,player_score)
+            elif menu_return == MENU_OPTION[3]:
+                Score.show(score)
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # closes window
                 quit()  # end pygame
